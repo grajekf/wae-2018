a65edcc29090497a5bac48a5d01d71da99fc4218
grajekf <filip.grajek@gmail.com>
2018-05-21 00:59:53 +0200

    Fixed repeat_ts

--- repeat_ts.sh
+++ repeat_ts.sh
@@ -6,6 +6,6 @@ for TS in 2 4 10 15 30
 do
     for I in {1..10}
     do
-        ./gen.py $1 -bs $BS -l results/${FILENAME}_bs_${BS}_${I}.csv -pat 10000 -b 50000
+        ./gen.py $1 -ts $TS -l results/${FILENAME}_ts_${TS}_${I}.csv -pat 10000 -b 50000
     done
 done
